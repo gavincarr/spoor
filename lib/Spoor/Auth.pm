f1b94ce899f1257a464edf9b2fb1b94b4e814dff
Gavin Carr <gavin@openfusion.com.au>
2011-10-04 12:48:07 +1100

    Change Spoor::Auth to use $ENV{SPOOR_HOME}.

--- lib/Spoor/Auth.pm
+++ lib/Spoor/Auth.pm
@@ -3,11 +3,8 @@ package Spoor::Auth;
 use strict;
 use File::Basename;
 
-# Set $SPOOR_HOME to our grandparent directory
-my $SPOOR_HOME = dirname dirname dirname $INC{"Spoor/Auth.pm"};
-
-sub dsn      { "dbi:SQLite:dbname=$SPOOR_HOME/var/spoor.db" }
-sub dsn_test { "dbi:SQLite:dbname=$SPOOR_HOME/var/spoor_test.db" }
+sub dsn      { "dbi:SQLite:dbname=$ENV{SPOOR_HOME}/var/spoor.db" }
+sub dsn_test { "dbi:SQLite:dbname=$ENV{SPOOR_HOME}/var/spoor_test.db" }
 
 1;
 
